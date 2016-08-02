40eaf76a043c46967e4188d11de94aa1e8ccab77
PohSayKeong <pohsaykeong@dhs.sg>
2016-08-02 18:42:53 +0800

    Replaced string-array with JSON

--- app/src/main/java/com/percepshunnn/voluntunity/MapScreenFragment.java
+++ app/src/main/java/com/percepshunnn/voluntunity/MapScreenFragment.java
@@ -26,6 +26,9 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -48,12 +51,7 @@ public class MapScreenFragment extends android.support.v4.app.Fragment implement
     private TextView eventAddress;
     private TextView eventRoles;
     private TextView eventSkills;
-    private Button signUp;
-    private TextView upcoming1;
 
-    String[][] mTestArray;
-
-    ArrayList<LatLng> locations = new ArrayList();
     @Override
     public void onResume() {
         super.onResume();
@@ -66,24 +64,13 @@ public class MapScreenFragment extends android.support.v4.app.Fragment implement
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_map, container, false);
         return v;
+
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        TypedArray ta = getResources().obtainTypedArray(R.array.events);
-        int n = ta.length();
-        String[][] array = new String[n][];
-        for (int i=0; i < n; i++) {
-            int id = ta.getResourceId(i, 0);
-            if (id > 0) {
-                array[i] = getResources().getStringArray(id);
-            }
-        }
-        ta.recycle();
-        mTestArray = array;
 
 
 
@@ -102,21 +89,32 @@ public class MapScreenFragment extends android.support.v4.app.Fragment implement
         eventAddress = (TextView) view.findViewById(R.id.eventAddress);
         eventRoles = (TextView) view.findViewById(R.id.eventRoles);
         eventSkills = (TextView) view.findViewById(R.id.eventSkills);
-        signUp = (Button) view.findViewById(R.id.signUp);
-        upcoming1 = (TextView) view.findViewById(R.id.upcoming1);
         Log.d("MapFragment", "onViewCreated");
+
+
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-
-        for(String[] event : mTestArray){
-            LatLng location = new LatLng(Double.parseDouble(event[1]), Double.parseDouble(event[2]));
-            googleMap.addMarker(new MarkerOptions()
-                    .position(location)
-                    .title(event[0]));
+        try {
+            String strJson = getResources().getString(R.string.eventsJson);
+            JSONObject jsonRootObject = new JSONObject(strJson);
+            JSONArray jsonArray = jsonRootObject.optJSONArray("events");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Double lat = Double.parseDouble(jsonObject.optString("lat").toString());
+                Double lng = Double.parseDouble(jsonObject.optString("lng").toString());
+                String title = jsonObject.optString("title").toString();
+                LatLng location = new LatLng(lat, lng);
+                googleMap.addMarker(new MarkerOptions()
+                        .position(location)
+                        .title(title));
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+            android.os.Process.killProcess(android.os.Process.myPid());
         }
 
 
@@ -147,23 +145,38 @@ public class MapScreenFragment extends android.support.v4.app.Fragment implement
     }
 
     public void displayEventDetails(final String id) {
-        String[] eventToDisplay = new String[0];
-
-        for (String[] event : mTestArray) {
-            if (event[0].equals(id)) {
-                eventToDisplay = event;
-                break;
+        String[] eventToDisplay = new String[8];
+        try {
+            String strJson = getResources().getString(R.string.eventsJson);
+            JSONObject jsonRootObject = new JSONObject(strJson);
+            JSONArray jsonArray = jsonRootObject.optJSONArray("events");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (jsonObject.optString("title").toString().equals(id)) {
+                    eventToDisplay[0] = jsonObject.optString("title").toString();
+                    eventToDisplay[1] = jsonObject.optString("org").toString();
+                    eventToDisplay[2] = jsonObject.optString("date").toString();
+                    eventToDisplay[3] = jsonObject.optString("time").toString();
+                    eventToDisplay[4] = jsonObject.optString("desc").toString();
+                    eventToDisplay[5] = jsonObject.optString("address").toString();
+                    eventToDisplay[6] = jsonObject.optString("roles").toString();
+                    eventToDisplay[7] = jsonObject.optString("skills").toString();
+                    break;
+                }
             }
-        }
-        if (eventToDisplay != null) {
-            eventName.setText(eventToDisplay[3]);
-            eventOrganization.setText(eventToDisplay[4]);
-            eventDate.setText(eventToDisplay[5]);
-            eventTime.setText(eventToDisplay[6]);
-            eventDescription.setText(eventToDisplay[7]);
-            eventAddress.setText(eventToDisplay[8]);
-            eventRoles.setText(eventToDisplay[9]);
-            eventSkills.setText(eventToDisplay[10]);
+            if (eventToDisplay != null) {
+                eventName.setText(eventToDisplay[0]);
+                eventOrganization.setText(eventToDisplay[1]);
+                eventDate.setText(eventToDisplay[2]);
+                eventTime.setText(eventToDisplay[3]);
+                eventDescription.setText(eventToDisplay[4]);
+                eventAddress.setText(eventToDisplay[5]);
+                eventRoles.setText(eventToDisplay[6]);
+                eventSkills.setText(eventToDisplay[7]);
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+            android.os.Process.killProcess(android.os.Process.myPid());
         }
 
     }
