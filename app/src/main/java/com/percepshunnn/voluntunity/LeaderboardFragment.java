eb418c89eba86777975eaffd4ad74f3a0a808c9a
kaine119 <muikaien1@gmail.com>
2016-08-03 20:33:56 +0800

    Add support to filter list of users from db by an array of userIds
    This new change grabs a list of users' friends, and filters the raw list
    of users from the db according to the user's friends. This effectively
    results in only the user's friends being shown.
    Unfortunately, since I have no friends (who use this app), I'm putting
    in a sample array at the moment.

--- app/src/main/java/com/percepshunnn/voluntunity/LeaderboardFragment.java
+++ app/src/main/java/com/percepshunnn/voluntunity/LeaderboardFragment.java
@@ -11,6 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,12 +23,15 @@ import com.google.firebase.database.ValueEventListener;
 import com.percepshunnn.voluntunity.leaderboardview.LeaderboardEntry;
 import com.percepshunnn.voluntunity.leaderboardview.LeaderboardEntryAdapter;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LeaderboardFragment extends android.support.v4.app.Fragment{
 
     List<LeaderboardEntry> entries = new ArrayList<>();
+    List<Long> friendsIdList = new ArrayList<>();
 
     // Things for the list (RecyclerView)
     RecyclerView mRecyclerView;
@@ -33,12 +40,56 @@ public class LeaderboardFragment extends android.support.v4.app.Fragment{
     // Loading bar
     ProgressBar mProgressBar;
 
+    // Database stuff
+    DatabaseReference usersRef;
+    
+    // TODO: remove this
+    String TAG = "LeaderboardFragment";
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.content_leaderboard, container, false);
 
+        Log.d(TAG, "onCreateView");
+
         // Instructions taken from http://hmkcode.com/android-simple-recyclerview-widget-example/
 
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+
+        // <editor-fold desc="Facebook get friends id">
+        GraphRequest request = GraphRequest.newMyFriendsRequest(AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONArrayCallback() {
+                    @Override
+                    public void onCompleted(JSONArray objects, GraphResponse response) {
+                        String TAG = "FacebookCallback";
+                        Log.d("FacebookCallback", "onCompleted: the request came through");
+                        Log.d(TAG, "onCompleted: friends are " + objects.toString());
+
+                        // If you uncomment this it should work, but I can't test it.
+//                        for (int i = 0; i < objects.length(); i++) {
+//                            try {
+//                                JSONObject user = objects.getJSONObject(i);
+//                                Log.d("FacebookCallback", "onCompleted: friend: " + user.toString());
+//                            } catch (JSONException e) {
+//                                Log.e("FacebookCallback", "onCompleted: something bad happened while trying to get leaderboard friends", e);
+//                            }
+//                        }
+                        // I have no friends, to I have to use a dummy array.
+                        // http://i.imgur.com/92Q62wf
+                        // If you check the db, this maps to a "Tan Bee Hong".
+                        friendsIdList.add(1101559985101051L);
+                    }
+                });
+
+        Bundle params = new Bundle();
+        params.putString("fields", "name, id");
+        request.setParameters(params);
+        request.executeAsync();
+        // </editor-fold>
+
+
+        // <editor-fold desc="Initialise leaderboard screen and view references">
         mProgressBar = (ProgressBar) rootView.findViewById(R.id.leaderboard_loading_bar);
+
 
         // 1. Get a reference to the RecyclerView
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.leaderboard_parent);
@@ -46,33 +97,6 @@ public class LeaderboardFragment extends android.support.v4.app.Fragment{
         // 2. Set LayoutManager
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Getting users from firebase
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference usersRef = database.getReference("users/");
-
-        usersRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // dataSnapshot is the whole encompassing list
-                // We'll have to iterate through each child to get the appropriate object
-                for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
-                    LeaderboardEntry user = userSnap.getValue(LeaderboardEntry.class);
-                    Log.d("FirebaseCallback", "onChildAdded: " + user.getHours());
-                    entries.add(user);
-
-                    // Update the recycler view that it has new things to show
-                    mAdapter.notifyDataSetChanged();
-
-                    mRecyclerView.setVisibility(View.VISIBLE);
-                    mProgressBar.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
 
 
         // 3. create an adapter
@@ -82,14 +106,94 @@ public class LeaderboardFragment extends android.support.v4.app.Fragment{
         // 5. Set item animator (...?) to DefaultAnimator
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-
-
-
-
-
+        // </editor-fold>
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        
+        // Initialise loading bar so it shows up upon resuming activity
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
+
+        // <editor-fold desc="Facebook get friends id and display ">
+
+        // Sometimes the database call comes back faster than the facebook call.
+        // This results in nothing being shown, because there's nothing in friendsIdList.
+        // Thus we have to chain one after the other.
+        // It's probably inefficient as all hell, but it works.
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        usersRef = database.getReference("users/");
+        GraphRequest request = GraphRequest.newMyFriendsRequest(AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONArrayCallback() {
+                    @Override
+                    public void onCompleted(JSONArray objects, GraphResponse response) {
+                        String TAG = "FacebookCallback";
+                        Log.d("FacebookCallback", "onCompleted: the request came through");
+                        Log.d(TAG, "onCompleted: friends are " + objects.toString());
+
+                        // If you uncomment this it should work, but I can't test it.
+//                        for (int i = 0; i < objects.length(); i++) {
+//                            try {
+//                                JSONObject user = objects.getJSONObject(i);
+//                                Log.d("FacebookCallback", "onCompleted: friend: " + user.toString());
+//                            } catch (JSONException e) {
+//                                Log.e("FacebookCallback", "onCompleted: something bad happened while trying to get leaderboard friends", e);
+//                            }
+//                        }
+
+                        // I have no friends, to I have to use a dummy array.
+                        // http://imgur.com/92Q62wf
+                        // If @kaine119 check (his) db, this maps to a "Tan Bee Hong".
+                        friendsIdList.add(1101559985101051L);
+
+                        // <editor-fold desc="Getting users from firebase">
+                        usersRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                // dataSnapshot is the whole encompassing list
+                                // We'll have to iterate through each child to get the appropriate object
+                                for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
+                                    LeaderboardEntry user = userSnap.getValue(LeaderboardEntry.class);
+                                    Log.d("FirebaseCallback", "onChildAdded: " + user.getHours());
+                                    // only include the entry if he's in the user's friends
+                                    if (friendsIdList.contains(user.getId())) {
+                                        entries.add(user);
+                                    }
+
+                                    // Update the recycler view that it has new things to show
+                                    mAdapter.notifyDataSetChanged();
+
+                                    mRecyclerView.setVisibility(View.VISIBLE);
+                                    mProgressBar.setVisibility(View.GONE);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                        //</editor-fold>
+                    }
+                });
+
+        Bundle params = new Bundle();
+        params.putString("fields", "name, id");
+        request.setParameters(params);
+        request.executeAsync();
+        // </editor-fold>
+
+
+
+
+
+        Log.d(TAG, "onViewCreated: loading bar should be visible");
+    }
 
 
 }
